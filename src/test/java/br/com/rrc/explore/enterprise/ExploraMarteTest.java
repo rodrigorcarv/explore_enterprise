c9ed7104dfa4034d48bf7479fe06b78405f8ede4
Rodrigo Carvalho <rodrigo.carvalho@fcamara.com.br>
2016-04-23 13:03:42 -0300

    Criando cenario de teste para missao e exploracao de marte

---
+++ src/test/java/br/com/rrc/explore/enterprise/ExploraMarteTest.java
@@ -0,0 +1,25 @@
+package br.com.rrc.explore.enterprise;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.rrc.explore.enterprise.service.MissaoService;
+
+public class ExploraMarteTest {
+	
+	@Test
+	public void testExploraMarte () {
+		
+		String [][] mapa = new String[6][6];
+		int posicaoEntradaX = 1;
+		int posicaoEntradaY = 2;
+		String direcao = "N";
+
+		String comandos = "LMLMLMLMM";
+
+		MissaoService missaoMarteService = new MissaoService();
+		String resultaMissao = missaoMarteService.explorar(mapa, posicaoEntradaX, posicaoEntradaY, direcao, comandos);
+		
+		Assert.assertEquals("1 3 N", resultaMissao);
+	}
+}
---
+++ src/test/java/br/com/rrc/explore/enterprise/service/MissaoService.java
@@ -0,0 +1,12 @@
+package br.com.rrc.explore.enterprise.service;
+
+public class MissaoService {
+
+
+	public String explorar(String[][] mapa, int posicaoEntradaX, int posicaoEntradaY, String direcao, String comandos) {
+
+		
+		return null;
+	}
+
+}
